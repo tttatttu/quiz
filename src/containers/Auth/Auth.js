71a1aa644507191e75c51254f114e689df3f9f1c
tttatttu <33tt33@mail.ru>
2021-07-21 23:18:08 +0300

    add authorization

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, {Component} from "react";
+import classes from './Auth.module.css';
+import Button from "../../components/UI/Button/Button";
+import Input from "../../components/UI/Input/Input";
 
-const Auth = () => (
-  <div>
-    <h1>Auth</h1>
-  </div>
-);
+const Auth  = () => {
+
+  const loginHandler = () => {
+
+  };
+
+  const registerHandler =() => {
+
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+  }
+
+
+  return (
+    <div className={classes.Auth}>
+      <div>
+        <h1>Авторизация</h1>
+
+        <form onSubmit={submitHandler} className={classes.AuthForm}>
+          <Input label='Email'/>
+          <Input label='Password' errorMessage='Test'/>
+
+
+          <Button type='success' onClick={loginHandler}>Войти</Button>
+          <Button type='primary' onClick={registerHandler}>Зарегистрироваться</Button>
+        </form>
+      </div>
+    </div>
+  );
+
+};
 
 export default Auth;
